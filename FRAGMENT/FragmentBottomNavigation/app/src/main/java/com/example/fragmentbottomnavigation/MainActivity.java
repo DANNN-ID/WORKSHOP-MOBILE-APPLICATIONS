4145dbbc64d20b74af3a0e25032be09aa0ba67ec
AHMAD WILDAN <75109884+DANNN-ID@users.noreply.github.com>
2021-10-14 12:06:39 +0700

    Add files via upload

---
+++ FRAGMENT/FragmentBottomNavigation/app/src/main/java/com/example/fragmentbottomnavigation/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.fragmentbottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatsFragment()).commit();
+
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_chat:
+                        selectedFragment = new ChatsFragment();
+                        break;
+                    case R.id.nav_status:
+                        selectedFragment = new StatusFragment();
+                        break;
+                    case R.id.nav_calls:
+                        selectedFragment = new CallsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
